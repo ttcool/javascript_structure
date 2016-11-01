fe07b76c6ff1e8044725f194ae8f978c18975fcf
System Administrator <ttcool@live.cn>
2016-11-01 18:48:15 +0800

    add List

---
+++ List.js
@@ -0,0 +1,23 @@
+function List() {
+    this.listSize = 0;
+    this.pos = 0;
+    this.dataStore = []; 
+    // 初始化一个空数组来保存列表元素 
+    this.clear = clear;
+    this.find = find;
+    this.toString = toString; 
+    this.insert = insert; 
+    this.append = append; 
+    this.remove = remove; 
+    this.front = front;
+    this.end = end;
+    this.prev = prev;
+    this.next = next;
+    this.length = length;
+    this.currPos = currPos;
+    this.moveTo = moveTo;
+    this.getElement = getElement;
+    this.length = length;
+    this.contains = contains;
+}
+
