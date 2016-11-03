db0bf0adb65acb0a9dbb9a9e6e472cb8a935fa1e
System Administrator <ttcool@live.cn>
2016-11-03 17:45:23 +0800

    add Queue

--- Queue.js
+++ Queue.js
@@ -118,6 +118,67 @@ if (femaleDancers.count() > 0) {
 }
 
 
+//对于 0~99 的数字,基数排序将数据集扫描两次。第一次按个位上的数字进行排序,第二 次按十位上的数字进行排序。
+//每个数字根据对应位上的数值被分在不同的盒子里。
+
+function distribute(nums, queues, n, digit) {
+        for (var i = 0; i < n; ++i) {
+           if (digit == 1) {
+              queues[nums[i]%10].enqueue(nums[i]);
+           }
+           else {
+              queues[Math.floor(nums[i] / 10)].enqueue(nums[i]); 
+           }
+               
+         } 
+}
+    
+function collect(queues, nums) {
+        var i = 0;
+        for (var digit = 0; digit < 10; ++digit) {
+           while (!queues[digit].empty()) {
+               nums[i++] = queues[digit].dequeue();
+           }
+        } 
+}
+
+function dispArray(arr) {
+        for (var i = 0; i < arr.length; ++i) {
+           console.log(arr[i] + " ");
+        }
+}
+
+
+//生成10个队列
+var queues = [];
+     
+for (var i = 0; i < 10; ++i) {
+     queues[i] = new Queue();
+ }
+ 
+//随机生成10个两位数字数组   
+var nums = [];
+     
+for (var i = 0; i < 10; ++i) {
+    nums[i] = Math.floor(Math.floor(Math.random() * 101)); 
+}
+
+
+console.log("Before radix sort: ");
+dispArray(nums);
+
+//对数组个位数字排序
+distribute(nums, queues, 10, 1);
+collect(queues, nums);
+
+//对数组十位数字排序
+distribute(nums, queues, 10, 10);
+collect(queues, nums);
+console.log("\n\nAfter radix sort: ");
+dispArray(nums);
+
+
+
 
 
 
