3abc11cc4732041227eccb0ed279f9a8938238a4
System Administrator <ttcool@live.cn>
2016-11-03 16:18:50 +0800

    add queue

--- Queue.js
+++ Queue.js
@@ -1,5 +1,7 @@
 //Queue 类
 
+var fs = require('fs');
+
 function Queue() {
         this.dataStore = [];
         this.enqueue = enqueue;
@@ -8,6 +10,7 @@ function Queue() {
         this.back = back;
         this.toString = toString;
         this.empty = empty;
+        this.count = count;
      }
 
 //enqueue() 方法向队尾添加一个元素
@@ -48,6 +51,64 @@ function empty() {
            return false;
     } 
 }
+
+//显示元素个数
+function count() {
+     return this.dataStore.length;
+}
+
+
+//
+
+function Dancer(name, sex) {
+   this.name = name;
+   this.sex = sex;
+}
+
+function getDancers(males, females) {
+   var names = fs.readFileSync('dancers.txt','utf-8').split("\n");
+   for (var i = 0; i < names.length; ++i) {
+      names[i] = names[i].trim();
+   }
+   for (var i = 0; i < names.length; ++i) {
+      var dancer = names[i].split(" ");
+      var sex = dancer[0];
+      var name = dancer[1];
+      if (sex == "F") {
+         females.enqueue(new Dancer(name, sex));
+      }
+      else {
+         males.enqueue(new Dancer(name, sex));
+      }
+    } 
+}
+
+
+function dance(males, females) {
+   console.log("The dance partners are: \n");
+   while (!females.empty() && !males.empty()) {
+      person = females.dequeue();
+      console.log("Female dancer is: " + person.name);
+      person = males.dequeue();
+      console.log(" and the male dancer is: " + person.name);
+   }
+   console.log();
+}
+
+var maleDancers = new Queue();
+var femaleDancers = new Queue();
+getDancers(maleDancers, femaleDancers);
+dance(maleDancers, femaleDancers);
+     
+if (maleDancers.count() > 0) {
+        console.log("There are " + maleDancers.count() + " male dancers waiting to dance.");
+     }
+     
+if (femaleDancers.count() > 0) {
+        console.log("There are " + femaleDancers.count() + " female dancers waiting to dance.");
+}
+
+
 
 
 
