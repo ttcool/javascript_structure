b7b4588714bb37d8ae198497bb93af30098c410f
System Administrator <ttcool@live.cn>
2016-11-03 16:33:27 +0800

    add Queue

--- Queue.js
+++ Queue.js
@@ -99,6 +99,15 @@ var maleDancers = new Queue();
 var femaleDancers = new Queue();
 getDancers(maleDancers, femaleDancers);
 dance(maleDancers, femaleDancers);
+
+if (!femaleDancers.empty()) {
+        console.log(femaleDancers.front().name + " is waiting to dance.");
+     }
+
+if (!maleDancers.empty()) {
+        console.log(maleDancers.front().name + " is waiting to dance.");
+     }
+
      
 if (maleDancers.count() > 0) {
         console.log("There are " + maleDancers.count() + " male dancers waiting to dance.");
